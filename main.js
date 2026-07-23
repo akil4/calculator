c078447aace610072a4ca0a7b362efce0dd2127c
Akil <113962713+akil4@users.noreply.github.com>
2026-07-23 19:53:06 +0530

    feat(calculator): implement full calculator functionality

--- main.js
+++ main.js
@@ -1,38 +1,78 @@
-a = "";
-b = "";
-operator = "";
+let a = "";
+let b = "";
+let operator = "";
+// console.log(a);
+// console.log(b);
+// console.log(operator);
+
+let operandButton = document.querySelectorAll(".operand");
+let operatorButton = document.querySelectorAll(".operator");
+let equalsButton = document.querySelector(".equals");
+let clearButton = document.querySelector(".clear");
+let textInput = document.querySelector(".calculator__input--text");
+
+operandButton.forEach(item => {
+    item.addEventListener("click", () => {
+        if (a !== "") {
+            b = item.value;
+            textInput.value += item.value;
+        }
+        else {
+            a = item.value;
+            textInput.value = item.value;
+        }
+    })
+});
+
+operatorButton.forEach(item => {
+    item.addEventListener("click", () => {
+        operator = item.value;
+        textInput.value += item.value;
+    })
+});
+
+equalsButton.addEventListener("click", () => {
+    operate(operator, a, b);
+});
+
+clearButton.addEventListener("click", () => {
+    textInput.value = "";
+    a = "";
+    b = "";
+    operator = "";
+})
 
 function operate(operator, a, b) {
     switch(operator) {
         case "+": 
-            add(a, b);
-            break;
+            return add(a, b);
         case "-": 
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case "*": 
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case "/": 
-            divide(a, b);
-            break;
+            return divide(a, b);
         default: 
             console.log("Enter a Valid Operator!!!!")
     }
 }
 
 function add (a, b) {
-    return a + b;
+    textInput.value = Number(a) + Number(b);
+    return Number(a) + Number(b);
 }
 
 function subtract (a, b) {
-    return a - b;
+    textInput.value = Number(a) - Number(b);
+    return Number(a) - Number(b);
 }
 
 function multiply (a, b) {
-    return a * b;
+    textInput.value = Number(a) * Number(b);
+    return Number(a) * Number(b);
 }
 
 function divide (a, b) {
-    return a / b;
+    textInput.value = Number(a) / Number(b);
+    return Number(a) / Number(b);
 }
