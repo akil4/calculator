6b8554b131f9eb3771d011f6890eebb82c86b8ea
Akil <113962713+akil4@users.noreply.github.com>
2026-07-23 12:55:27 +0530

    feat(calculator): add variables for operation components and implement operate function for calculations

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+a = "";
+b = "";
+operator = "";
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+": 
+            add(a, b);
+            break;
+        case "-": 
+            subtract(a, b);
+            break;
+        case "*": 
+            multiply(a, b);
+            break;
+        case "/": 
+            divide(a, b);
+            break;
+        default: 
+            console.log("Enter a Valid Operator!!!!")
+    }
+}
+
 function add (a, b) {
     return a + b;
 }
